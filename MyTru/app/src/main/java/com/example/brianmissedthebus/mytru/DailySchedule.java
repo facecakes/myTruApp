a81ff79293a0317377d2da8a3926d051d1b3b60e
darkhobbo <gregpetersen@hotmail.com>
2015-11-15 23:52:32 -0800

    Major changes, Added MOCK database with 5 students and over 20 courses. Added user authentication. Added user info storage. User info can be access via shared pref: STUDENT_SCHEDULE STUDENT_SCHEDULE has "Student" holding ID and "MyCourses" string set holding their courses.
    Minor changes,
    Cleaned switch structures.
    Removed useless code.

--- MyTru/app/src/main/java/com/example/brianmissedthebus/mytru/DailySchedule.java
+++ MyTru/app/src/main/java/com/example/brianmissedthebus/mytru/DailySchedule.java
@@ -2,9 +2,6 @@ package com.example.brianmissedthebus.mytru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.Window;
 
 public class DailySchedule extends AppCompatActivity {
 
@@ -12,27 +9,5 @@ public class DailySchedule extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_schedule);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_daily_schedule, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
