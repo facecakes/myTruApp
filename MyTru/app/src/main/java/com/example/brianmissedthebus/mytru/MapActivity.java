60c0193f7de6f8551e656c1648fde15c758f6418
darkhobbo <gregpetersen@hotmail.com>
2015-11-18 14:35:17 -0800

    Branch commit test.
    Removing useless code.

--- MyTru/app/src/main/java/com/example/brianmissedthebus/mytru/MapActivity.java
+++ MyTru/app/src/main/java/com/example/brianmissedthebus/mytru/MapActivity.java
@@ -44,21 +44,6 @@ public class MapActivity extends AppCompatActivity implements View.OnClickListen
         toast.show();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void onClick(View v){
 
         switch(v.getId()){
